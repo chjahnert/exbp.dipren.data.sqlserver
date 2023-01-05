881a0f01a6f780ebdab067ff423517eb401296ed
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2023-01-05 15:00:27 +0100

    Implements a set of benchmarking tests for performance optimizations (#13)

--- source/EXBP.Dipren.Data.SqlServer.Tests/SqlServerEngineDataStoreTests.cs
+++ source/EXBP.Dipren.Data.SqlServer.Tests/SqlServerEngineDataStoreTests.cs
@@ -1,11 +1,7 @@
 ﻿
-using System.Globalization;
-
 using EXBP.Dipren.Data.Tests;
 
 using Microsoft.Data.SqlClient;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Smo;
 
 using NUnit.Framework;
 
@@ -14,11 +10,6 @@ namespace EXBP.Dipren.Data.SqlServer.Tests
 {
     internal class SqlServerEngineDataStoreTests : EngineDataStoreTests
     {
-        private const string CONNECTION_STRING_TEMPLATE = "Server = localhost; User Id = sa; Password = 4Laqzjn!LNYa@W63; TrustServerCertificate = True";
-
-        private const string PATH_SCHEMA_SCRIPT_INSTALL = @"../../../../Database/install.sql";
-        private const string PATH_SCHEMA_SCRIPT_REMOVE = @"../../../../Database/remove.sql";
-
         private const string DATABASE_NAME_MASTER = "master";
         private const string DATABASE_NAME_DIPREN = "dipren";
 
@@ -28,8 +19,8 @@ namespace EXBP.Dipren.Data.SqlServer.Tests
 
         public SqlServerEngineDataStoreTests()
         {
-            SqlConnectionStringBuilder builderMaster = new SqlConnectionStringBuilder(CONNECTION_STRING_TEMPLATE);
-            SqlConnectionStringBuilder builderDipren = new SqlConnectionStringBuilder(CONNECTION_STRING_TEMPLATE);
+            SqlConnectionStringBuilder builderMaster = new SqlConnectionStringBuilder(Database.ConnectionStringTemplate);
+            SqlConnectionStringBuilder builderDipren = new SqlConnectionStringBuilder(Database.ConnectionStringTemplate);
 
             builderMaster.InitialCatalog = DATABASE_NAME_MASTER;
             builderDipren.InitialCatalog = DATABASE_NAME_DIPREN;
@@ -39,86 +30,34 @@ namespace EXBP.Dipren.Data.SqlServer.Tests
         }
 
 
-        protected override Task<IEngineDataStore> OnCreateEngineDataStoreAsync()
-            => Task.FromResult<IEngineDataStore>(new SqlServerEngineDataStore(this._connectionStringDipren));
-
 
         [OneTimeSetUp]
         public async Task BeforeFirstTestCaseAsync()
         {
-            await this.DropDatabaseAsync();
-            await this.CreateDatabaseAsync();
+            await Database.DropDatabaseAsync(this._connectionStringMaster, DATABASE_NAME_DIPREN);
+            await Database.CreateDatabaseAsync(this._connectionStringMaster, DATABASE_NAME_DIPREN);
         }
 
         [SetUp]
         public async Task BeforeEachTestCaseAsync()
         {
-            await this.DropDatabaseSchemaAsync();
-            await this.CreateDatabaseSchemaAsync();
+            await Database.DropDatabaseSchemaAsync(this._connectionStringDipren);
+            await Database.CreateDatabaseSchemaAsync(this._connectionStringDipren);
         }
 
         [TearDown]
         public async Task AfterTestFixtureAsync()
         {
-            await this.DropDatabaseSchemaAsync();
+            await Database.DropDatabaseSchemaAsync(this._connectionStringDipren);
         }
 
         [OneTimeTearDown]
         public async Task AfterLastTestCaseAsync()
         {
-            await this.DropDatabaseAsync();
+            await Database.DropDatabaseAsync(this._connectionStringMaster, DATABASE_NAME_DIPREN);
         }
 
-
-        private async Task CreateDatabaseAsync(CancellationToken cancellation = default)
-        {
-            string statementCreate = string.Format(CultureInfo.InvariantCulture, SqlServerEngineDataStoreResources.QueryCreateDatabase, DATABASE_NAME_DIPREN);
-
-            await this.ExecuteStatementAsync(this._connectionStringMaster, statementCreate, cancellation);
-        }
-
-        private async Task DropDatabaseAsync(CancellationToken cancellation = default)
-        {
-            string statement = string.Format(CultureInfo.InvariantCulture, SqlServerEngineDataStoreResources.QueryDropDatabase, DATABASE_NAME_DIPREN);
-
-            await this.ExecuteStatementAsync(this._connectionStringMaster, statement, cancellation);
-        }
-
-        private async Task CreateDatabaseSchemaAsync(CancellationToken cancellation = default)
-            => await this.ExecuteScriptAsync(this._connectionStringDipren, PATH_SCHEMA_SCRIPT_INSTALL, cancellation);
-
-        private async Task DropDatabaseSchemaAsync(CancellationToken cancellation = default)
-            => await this.ExecuteScriptAsync(this._connectionStringDipren, PATH_SCHEMA_SCRIPT_REMOVE, cancellation);
-
-        private async Task ExecuteScriptAsync(string connectionString, string path, CancellationToken cancellation = default)
-        {
-            string script = await File.ReadAllTextAsync(path, cancellation);
-
-            await using SqlConnection clientConnection = new SqlConnection(connectionString);
-
-            await clientConnection.OpenAsync(cancellation);
-
-            ServerConnection serverConnection = new ServerConnection(clientConnection);
-            Server server = new Server(serverConnection);
-
-            server.ConnectionContext.BeginTransaction();
-            server.ConnectionContext.ExecuteNonQuery(script);
-            server.ConnectionContext.CommitTransaction();
-        }
-
-        private async Task ExecuteStatementAsync(string connectionString, string text, CancellationToken cancellation = default)
-        {
-            await using SqlConnection connection = new SqlConnection(connectionString);
-
-            await connection.OpenAsync(cancellation);
-
-            await using SqlCommand command = new SqlCommand
-            {
-                CommandText = text,
-                Connection = connection
-            };
-
-            await command.ExecuteNonQueryAsync(cancellation);
-        }
+        protected override Task<IEngineDataStore> OnCreateEngineDataStoreAsync()
+            => Task.FromResult<IEngineDataStore>(new SqlServerEngineDataStore(this._connectionStringDipren));
     }
 }
